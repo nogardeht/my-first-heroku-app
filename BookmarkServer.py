01e953c5d5013819c1019090caff7eb800f10324
Diego Hernan Marciano <thedragonsreborn@hotmail.com>
2017-07-20 13:31:06 -0300

    Modified code to use threading

--- BookmarkServer.py
+++ BookmarkServer.py
@@ -41,6 +41,8 @@
 import http.server
 import requests
 from urllib.parse import unquote, parse_qs
+import threading
+from socketserver import ThreadingMixIn
 import os
 
 memory = {}
@@ -147,9 +149,12 @@ class Shortener(http.server.BaseHTTPRequestHandler):
 </body>
 """.encode())
 
+class ThreadHTTPServer(ThreadingMixIn, http.server.HTTPServer):
+    "This is an HTTPServer that supports thread-based concurrency."
+
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8000))   # Use PORT if it's there.
-    print(str(port))
     server_address = ('', port)
-    httpd = http.server.HTTPServer(server_address, Shortener)
+    httpd = ThreadHTTPServer(server_address, Shortener)
     httpd.serve_forever()
